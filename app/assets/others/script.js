72344338314096a55d5423d0fd42f7a172c0d1f7
xuchunhong <317650137@qq.com>
2017-10-04 14:33:14 +0800

    - 实现在对比阅读圣经界面中选择性显示译本

--- app/assets/others/script.js
+++ app/assets/others/script.js
@@ -12,4 +12,18 @@ $(document).ready(function(){
     $('.prevchapter').click(function() {
         window.brcore.navPrevChapter();
     });
+
+    // toggle bible versions in comparing browser
+    $('button.bible').click(function() {
+        if ($(this).hasClass('active')) {
+            // remove active class and remove all this bible version's verses
+            $(this).removeClass('active');
+            $('th[value='+$(this).val()+']').hide();
+            $('td[value='+$(this).val()+']').hide();
+        } else {
+            $(this).addClass('active');
+            $('th[value='+$(this).val()+']').show();
+            $('td[value='+$(this).val()+']').show();
+        }
+    });
 });
