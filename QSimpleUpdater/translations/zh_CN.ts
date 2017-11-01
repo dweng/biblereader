2ed7c1f1d565591cc630d01f8011842859e70a68
Xuchunhong <317650137@qq.com>
2017-11-01 12:52:17 +0800

    auto update implemented

---
+++ QSimpleUpdater/translations/zh_CN.ts
@@ -0,0 +1,183 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>Downloader</name>
+    <message>
+        <location filename="../src/Downloader.ui" line="20"/>
+        <location filename="../src/Downloader.cpp" line="252"/>
+        <source>Updater</source>
+        <translation>升级</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.ui" line="87"/>
+        <location filename="../src/Downloader.cpp" line="115"/>
+        <location filename="../src/Downloader.cpp" line="320"/>
+        <source>Downloading updates</source>
+        <translation>正在下载更新</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.ui" line="110"/>
+        <source>Time remaining: 0 minutes</source>
+        <translation>剩余时间：0 分钟</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.ui" line="151"/>
+        <source>Open</source>
+        <translation>打开</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.ui" line="158"/>
+        <location filename="../src/Downloader.cpp" line="114"/>
+        <source>Stop</source>
+        <translation>停止</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="116"/>
+        <location filename="../src/Downloader.cpp" line="397"/>
+        <source>Time remaining</source>
+        <translation>剩余时间</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="116"/>
+        <source>unknown</source>
+        <translation>未知</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="193"/>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="194"/>
+        <source>Cannot find downloaded update!</source>
+        <translation>不能找到已下载的更新！</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="213"/>
+        <source>Close</source>
+        <translation>关闭</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="214"/>
+        <source>Download complete!</source>
+        <translation>下载完成！</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="215"/>
+        <source>The installer will open separately</source>
+        <translation>安装程序将打开</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="223"/>
+        <source>Click &quot;OK&quot; to begin installing the update</source>
+        <translation>点击“确认”来安装更新</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="225"/>
+        <source>In order to install the update, you may need to quit the application.</source>
+        <translation>为了要安装更新，你可能需要退出程序。</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="239"/>
+        <source>Click the &quot;Open&quot; button to apply the update</source>
+        <translation>点击“打开”按钮应用更新</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="255"/>
+        <source>Are you sure you want to cancel the download?</source>
+        <translation>你确认要取消下载吗？</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="303"/>
+        <location filename="../src/Downloader.cpp" line="312"/>
+        <source>%1 bytes</source>
+        <translation>%1 bytes</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="306"/>
+        <location filename="../src/Downloader.cpp" line="315"/>
+        <source>%1 KB</source>
+        <translation>%1 KB</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="309"/>
+        <location filename="../src/Downloader.cpp" line="318"/>
+        <source>%1 MB</source>
+        <translation>%1 MB</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="321"/>
+        <source>of</source>
+        <translation>/ </translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="345"/>
+        <source>Downloading Updates</source>
+        <translation>下载更新</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="347"/>
+        <source>Time Remaining</source>
+        <translation>剩余时间</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="348"/>
+        <source>Unknown</source>
+        <translation>未知</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="373"/>
+        <source>about %1 hours</source>
+        <translation>大约 %1 小时</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="375"/>
+        <source>about one hour</source>
+        <translation>大约 1 小时</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="383"/>
+        <source>%1 minutes</source>
+        <translation>%1 分钟</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="385"/>
+        <source>1 minute</source>
+        <translation>1 分钟</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="392"/>
+        <source>%1 seconds</source>
+        <translation>%1 秒</translation>
+    </message>
+    <message>
+        <location filename="../src/Downloader.cpp" line="394"/>
+        <source>1 second</source>
+        <translation>1 秒</translation>
+    </message>
+</context>
+<context>
+    <name>Updater</name>
+    <message>
+        <location filename="../src/Updater.cpp" line="413"/>
+        <source>Would you like to download the update now?</source>
+        <translation>需要现在下载更新吗？</translation>
+    </message>
+    <message>
+        <location filename="../src/Updater.cpp" line="415"/>
+        <source>Version %1 of %2 has been released!</source>
+        <translation>版本 %1 之 %2 已经发布！</translation>
+    </message>
+    <message>
+        <location filename="../src/Updater.cpp" line="441"/>
+        <source>No updates are available for the moment</source>
+        <translation>现在没有可用更新</translation>
+    </message>
+    <message>
+        <location filename="../src/Updater.cpp" line="443"/>
+        <source>Congratulations! You are running the latest version of %1</source>
+        <translation>恭喜！你运行的是最新版本的%1</translation>
+    </message>
+</context>
+</TS>
